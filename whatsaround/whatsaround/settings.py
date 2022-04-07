3ef335f5312434ae9e0ca271fd081ec1ffdd0a46
FIA1607 <65461525+FIA1607@users.noreply.github.com>
2022-04-08 01:34:47 +0700

    1234

--- whatsaround/whatsaround/settings.py
+++ whatsaround/whatsaround/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'whatsaround.urls'
@@ -161,6 +162,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = (BASE_DIR / 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'photos')
